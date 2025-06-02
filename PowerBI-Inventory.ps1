ccf4b0a31a18ad69631300b69ba69292d07b2fef
philperrin <35937414+philperrin@users.noreply.github.com>
2025-06-02 08:36:29 -0600

    Add files via upload

---
+++ PowerBI-Inventory.ps1
@@ -0,0 +1,112 @@
+#--------------------------------------------------------------------------------------------------
+# Script created by Phil Perrin with much support from Captain Google.
+# Last updated 5/25/25
+# Version: 1.0
+#
+# Purpose:
+# Query Power BI Service to extract information about the workspaces (groups) and reports in each.
+# Does not include 'My Workspace'.
+#
+# Instructions:
+# 1. Run PowerShell as admin.
+# 2. First time use: Install the Power BI PowerShell cmdlets (Install-Module MicrosoftPowerBIMgmt)
+#    https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps.
+# 3. Adjust parameters as needed.
+# 4. Run the script - user will be prompted for the Power BI account to use.
+# 5. Optional: Give Phil some feedback on how to make this better.
+#
+#--------------------------------------------------------------------------------------------------
+
+# Parameters:
+# Diagnostic mode: $false = deletes all temp files; $true = keeps all temp files. Default is $false.
+$diagnostic = $false
+
+# Create helper directories (directories and contents will be deleted if $diagnostic = FALSE):
+New-Item -ItemType Directory -Path .\helpers -Force | Out-Null
+New-Item -ItemType Directory -Path .\newjsons -Force | Out-Null
+New-Item -ItemType Directory -Path .\reports -Force | Out-Null
+New-Item -ItemType Directory -Path .\workspaces -Force | Out-Null
+
+# References for files we will be using throughout the script.
+$pbigroupsjson = ".\helpers\pbigroups.json"
+$pbigroups2json = ".\helpers\pbigroups2.json"
+$groupslist = ".\helpers\groups_list.csv"
+$groupslistfinal = ".\helpers\groups_list_final.csv"
+$workspacelist = ".\workspace_list.csv"
+$workspacereports = ".\workspace_reports.csv"
+
+# Connect to Power BI. User will be prompted to select the account to use.
+Connect-PowerBIServiceAccount
+
+# Create an object that stores all the group/workspace info.
+$allGroups = Invoke-PowerBIRestMethod -Url 'groups' -Method Get
+
+# Saves all the group/workspace info as a json.
+Out-File -FilePath $pbigroupsjson -InputObject $allGroups
+
+# Get the count of groups/workspaces (used in odata removal)
+$jsonString = Get-Content -Path $pbigroupsjson -Raw | ConvertFrom-Json
+$nworkspaces = $jsonString.'@odata.count'
+
+# The json we just created is helpful - but let's remove the @odata info. And then export results to csv.
+$pbistrip = " `"`@odata.context`":`"https://wabi-west-us-d-primary-redirect.analysis.windows.net/v1.0/myorg/`$metadata#groups`",`"`@odata.count`":$nworkspaces,`"value`":"
+(Get-Content $pbigroupsjson).Replace($pbistrip, "") | Set-Content $pbigroups2json
+(Get-Content $pbigroups2json).Substring(1) | Set-Content $pbigroups2json
+$pbigroups = Get-Content -Path $pbigroups2json -Raw | ConvertFrom-Json
+$pbigroups | Select-Object -Property id | Export-Csv -Path $groupslist -NoTypeInformation
+
+# Create a full list of workspaces with names etc.
+$pbigroups | Select-Object -Property id, isReadOnly, isOnDedicatedCapacity, capacityId, defaultDatasetStorageFormat, type, name | Export-Csv -Path $workspacelist -NoTypeInformation
+Get-Content -Path $groupslist | Select-Object -Skip 1 |  Out-File -FilePath $groupslistfinal
+# The last bit in here just makes a file that doesn't have a header and only has the workspace ids. That is all we need for the next steps.
+
+# Import the workspace ids to iterate through
+# Pro-Tip: create a subset list for troubleshooting.
+$groups = Import-Csv $groupslistfinal -Header id
+
+# Loop through each workspace id, call Power BI to get the report listing, and export the results from each to separate json files
+foreach ($row in $groups) {
+    $workspaceid = $row.id
+    $groupUrl = "groups/$workspaceid/reports"
+    $reports = Invoke-PowerBIRestMethod -Url $groupUrl -Method Get
+    Out-File -FilePath .\workspaces\$workspaceid.json -InputObject $reports
+}
+
+# So now we have a folder full of json files. Let's go through each one, clean them a bit, and convert the json to csv. 
+Get-ChildItem ".\workspaces" -Filter *.json | 
+Foreach-Object {
+    $jsonid = $_.BaseName
+    $firststrip = "  `"`@odata.context`":`"https://wabi-west-us-d-primary-redirect.analysis.windows.net/v1.0/myorg/groups/$jsonid/`$metadata#reports`",`"value`":"
+    (Get-Content .\workspaces\$jsonid.json).Replace($firststrip, "[") | Set-Content .\newjsons\$jsonid.json
+    (Get-Content .\newjsons\$jsonid.json).Substring(1) | Set-Content .\newjsons\$jsonid.json
+    $json = Get-Content -Path ".\newjsons\$jsonid.json" -Raw | ConvertFrom-Json
+    $json | Select-Object -Property id, reportType, name, webUrl, embedUrl, isFromPbix, isOwnedByMe, datasetId, datasetWorkspaceId | Export-Csv -Path ".\reports\$jsonid.csv" -NoTypeInformation
+}
+# Watch for errors during the run! Some files might have something funky in there and you may need to manually re-run one.
+
+# Append all the results into one csv. Also cleans up extra headers in the content.
+Get-Content ".\reports\*.csv" | 
+Add-Content $workspacereports
+$cleanupreportfile = Import-Csv -Path $workspacereports
+$filteredData = $cleanupreportfile | Where-Object { $_.id -ne "id" }
+$filteredData | Export-Csv -Path $workspacereports -NoTypeInformation
+
+# One last update to the reports file - let's extract the report workspace id into a separate column.
+$addreportworkspace = Import-Csv -Path $workspacereports
+foreach ($row in $addreportworkspace) {
+    $row | Add-Member -MemberType NoteProperty -Name "report_workspace_id" -Value $row.webUrl.Substring(31,36) -Force
+}
+$addreportworkspace | Export-Csv -Path $workspacereports -NoTypeInformation
+
+
+# If NOT in diagnostic mode then delete directories and contents: helpers, newjsons, reports, workspaces
+if ($diagnostic) {
+    Write-Host ">>> Diagnostic Mode: All files retained. You may want to clean these up manually before running again."
+}
+else {
+    Remove-Item -Path ".\helpers" -Force -Recurse
+    Remove-Item -Path ".\newjsons" -Force -Recurse
+    Remove-Item -Path ".\reports" -Force -Recurse
+    Remove-Item -Path ".\workspaces" -Force -Recurse
+    Write-Host ">>> Script complete and support files removed. You should have 2 csv files with workspace and report info."
+}
---
+++ PowerBI-RefreshSchedules.ps1
@@ -0,0 +1,138 @@
+﻿#--------------------------------------------------------------------------------------------------
+# Script created by Phil Perrin with much support from Captain Google.
+# Last updated 5/31/25
+# Version: 1.0
+#
+# Purpose:
+# Query Power BI Service to extract the refresh schedules of each dataset in prod workspaces
+# Does not include 'My Workspace' or non-Prod workspaces. Does not include Usage Metric reports.
+#
+# Instructions:
+# 1. Run PowerShell as admin.
+# 2. First time use: 
+#    a. Install the Power BI PowerShell cmdlets (Install-Module MicrosoftPowerBIMgmt)
+#       https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps.
+#    b. Install the Join-Object cmdlets (Install-Module -Name Join-Object)
+# 3. Adjust parameters as needed.
+# 4. Run the script - user will be prompted for the Power BI account to use.
+# 5. Optional: Give Phil some feedback on how to make this better.
+#
+#--------------------------------------------------------------------------------------------------
+
+# Connect to Power BI. User will be prompted to select the account to use.
+Connect-PowerBIServiceAccount
+
+# First, get workspaces and filter out so only prod
+Write-Host "Gathering workspaces and filtering out dev/qa/pocs."
+$allGroups = Invoke-PowerBIRestMethod -Url 'groups' -Method Get
+$allGroups = $allGroups | ConvertFrom-Json
+
+$w_data = foreach ($row in $allGroups.value){
+    [PSCustomObject] @{
+    w_id = $row.id
+    w_name = $row.name
+    }
+}
+
+$w_data = $w_data | Where-Object { $_.w_name -notlike "*dev*" }
+$w_data = $w_data | Where-Object { $_.w_name -notlike "*qa*" }
+$w_data = $w_data | Where-Object { $_.w_name -notlike "*poc*" }
+Write-Host "Done!"
+
+$w_datacount = $w_data.Count
+
+# Now that we have the list of workspaces we need, let's get the datasets in each.
+Write-Host "Getting datasets from each workspace ($w_datacount)."
+$w_datasets = foreach ($row in $w_data){
+    $g_id = $row.w_id
+    $w_name = $row.group
+    $w_result = Invoke-PowerBIRestMethod -Url "groups/$g_id/datasets" -Method Get
+    $w_result = $w_result | ConvertFrom-Json
+    
+    [PSCustomObject] @{
+        d_value = $w_result.value
+    }
+}
+
+# A bit of a mess, but with that data, let's convert the output into something usable - keeping the dataset, group id, name and if the dataset is refreshable.
+$dataset_obj = foreach ($row in $w_datasets.d_value){
+[PSCustomObject] @{
+        d_id = $row.id
+        d_name = $row.name
+        w_id = $row.webUrl.Substring(31,36)
+        isRefreshable = $row.isRefreshable
+    }
+}
+Write-Host "Done!"
+
+# Keep only the refreshable datasets
+$dataset_obj2 = $dataset_obj | Where-Object { $_.isRefreshable -notlike "FALSE" }
+$dataset_obj2 = $dataset_obj2 | Where-Object { $_.d_name -notlike "*usage metrics*"}
+
+# Join the group name back to dataset_obj
+$dataset_obj2 = $dataset_obj2 | LeftJoin $w_data -On w_id
+
+$ds_o2_count= $dataset_obj2.count
+
+Write-Host "Getting refresh schedule info from each dataset ($ds_o2_count)."
+# Using the dataset id and the group id, pull the refresh schedule on each dataset that is refreshable.
+$allscheds = foreach ($roww in $dataset_obj2) {
+    $workspaceid = $roww.w_id
+    $datasetid = $roww.d_id
+    $dataset_name = $roww.d_name
+    $workspace_name = $roww.w_name
+
+    $refreshscheds = Invoke-PowerBIRestMethod -Url "groups/$workspaceid/datasets/$datasetid/refreshSchedule" -Method Get
+    $refreshschedsconv = $refreshscheds | ConvertFrom-Json
+    
+    [PSCustomObject] @{
+        datasetid = $datasetid
+        workspace_id = $workspaceid
+        dataset_name = $dataset_name
+        workspace_name = $workspace_name
+        enabled = $refreshschedsconv.enabled
+        localTimeZoneId = $refreshschedsconv.localTimeZoneId
+        notifyOption = $refreshschedsconv.notifyOption
+        refresh_days = $refreshschedsconv.days
+        refresh_times = $refreshschedsconv.times
+        }
+}
+Write-Host "Done!"
+
+Write-Host "Let's put all the refresh times into one column."
+$allscheds_time = foreach($row in $allscheds){
+    $datasetid = $row.datasetid
+    $workspace_id = $row.workspace_id
+    $dataset_name = $row.dataset_name
+    $workspace_name = $row.workspace_name
+    $enabled = $row.enabled
+    $localTimeZoneId = $row.localTimeZoneId
+    $notifyOption = $row.notifyOption
+
+    foreach($inner in $row.refresh_times){
+        $r_times = $inner
+
+        foreach($inner2 in $row.refresh_days){
+            $r_days = $inner2
+
+            [PSCustomObject] @{
+                datasetid = $datasetid
+                workspace_id = $workspace_id
+                dataset_name = $dataset_name
+                workspace_name = $workspace_name
+                enabled = $enabled
+                localTimeZoneId = $localTimeZoneId
+                notifyOption = $notifyOption
+                r_days = $r_days
+                r_times = $r_times
+                }
+            }
+        }
+    }
+
+$result_count = $allscheds_time.count
+Write-Host "Done! Exporting results ($result_count)."
+# Export results to file.
+$allscheds_time | Select-Object -Property datasetid,dataset_name,workspace_id,workspace_name,enabled,localTimeZoneId,notifyOption,r_days,r_times | Export-Csv -Path ".\dataset_refresh_schedule.csv"  -NoTypeInformation -Force
+
+Write-Host "Output complete. Please find the file 'dataset_refresh_schedule.csv' in the local directory."
