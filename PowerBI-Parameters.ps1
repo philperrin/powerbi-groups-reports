d593f3dc2b1c810b32008e27168d576bcec1d3df
philperrin <35937414+philperrin@users.noreply.github.com>
2025-06-13 20:34:52 -0600

    Add files via upload

--- PowerBI-Parameters.ps1
+++ PowerBI-Parameters.ps1
@@ -4,15 +4,14 @@
 # Version: 1.0
 #
 # Purpose:
-# Query Power BI Service to extract the parameters from each dataset in prod workspaces.
-# Does not include 'My Workspace' or non-Prod workspaces. Does not include Usage Metric reports.
+# Query Power BI Service to extract the parameters from each dataset in workspaces.
+# Does not include 'My Workspace'. Does not include Usage Metric reports.
 #
 # Instructions:
 # 1. Run PowerShell as admin.
 # 2. First time use: 
 #    a. Install the Power BI PowerShell cmdlets (Install-Module MicrosoftPowerBIMgmt)
 #       https://docs.microsoft.com/en-us/powershell/power-bi/overview?view=powerbi-ps.
-#    b. Install the Join-Object cmdlets (Install-Module -Name Join-Object)
 # 3. Adjust parameters as needed.
 # 4. Run the script - user will be prompted for the Power BI account to use.
 # 5. Optional: Give Phil some feedback on how to make this better.
@@ -24,8 +23,7 @@ Connect-PowerBIServiceAccount
 
 # First, get workspaces and filter out so only prod
 Write-Host "Gathering workspaces and filtering out dev/qa/pocs."
-$allGroups = Invoke-PowerBIRestMethod -Url 'groups' -Method Get
-$allGroups = $allGroups | ConvertFrom-Json
+$allGroups = Invoke-PowerBIRestMethod -Url 'groups' -Method Get | ConvertFrom-Json
 
 $w_data = foreach ($row in $allGroups.value){
     [PSCustomObject] @{
@@ -34,9 +32,6 @@ $w_data = foreach ($row in $allGroups.value){
     }
 }
 
-$w_data = $w_data | Where-Object { $_.w_name -notlike "*dev*" }
-$w_data = $w_data | Where-Object { $_.w_name -notlike "*qa*" }
-$w_data = $w_data | Where-Object { $_.w_name -notlike "*poc*" }
 Write-Host "Done!"
 
 $w_datacount = $w_data.Count
@@ -47,62 +42,75 @@ Write-Host "Getting datasets from each workspace ($w_datacount)."
 
 $w_datasets = foreach ($row in $w_data){
     $g_id = $row.w_id
-    $w_name = $row.group
-    $w_result = Invoke-PowerBIRestMethod -Url "groups/$g_id/datasets" -Method Get
-    $w_result = $w_result | ConvertFrom-Json
+    $w_name = $row.w_name
+    $w_result = Invoke-PowerBIRestMethod -Url "groups/$g_id/datasets" -Method Get | ConvertFrom-Json
     
     [PSCustomObject] @{
+        w_id = $g_id
+        w_name = $w_name
         d_value = $w_result.value
     }
 }
 
 # A bit of a mess, but with that data, let's convert the output into something usable - keeping the dataset, group id, name and if the dataset is refreshable.
-$dataset_obj = foreach ($row in $w_datasets.d_value){
-[PSCustomObject] @{
-        d_id = $row.id
-        d_name = $row.name
-        w_id = $row.webUrl.Substring(31,36)
-        isRefreshable = $row.isRefreshable
-    }
-}
+$dataset_obj = foreach ($row in $w_datasets){
+    $w_id = $row.w_id
+    $w_name = $row.w_name
+    $d_array = $row.d_value
+    foreach($row2 in $d_array){
+        $d_id = $row2.id
+        $d_name = $row2.name
+        $d_refresh = $row2.isRefreshable
+            [PSCustomObject] @{
+                w_id = $w_id
+                w_name = $w_name
+                d_id = $d_id
+                d_name = $d_name
+                d_refresh = $d_refresh
+                }
+            }
+        }
+
 Write-Host "Done!"
 
 # Keep only the refreshable datasets
 $dataset_obj2 = $dataset_obj | Where-Object { $_.isRefreshable -notlike "FALSE" }
 $dataset_obj2 = $dataset_obj2 | Where-Object { $_.d_name -notlike "*usage metrics*"}
 
-# Join the group name back to dataset_obj
-$dataset_obj2 = $dataset_obj2 | LeftJoin $w_data -On w_id
 
-$ds_o2_count= $dataset_obj2.count
+$ds_o2_count = $dataset_obj2.count
 
 Write-Host "Getting parameter values info from each dataset ($ds_o2_count)."
 # Using the dataset id and the group id, pull the parameters from each dataset.
 $param_values = foreach ($roww in $dataset_obj2) {
-    $workspaceid = $roww.w_id
-    $datasetid = $roww.d_id
-    $dataset_name = $roww.d_name
-    $workspace_name = $roww.w_name
-
-    $parameters = Invoke-PowerBIRestMethod -Url "groups/$workspaceid/datasets/$datasetid/parameters" -Method Get
-    $parameters = $parameters | ConvertFrom-Json
+    $w_id = $roww.w_id
+    $d_id = $roww.d_id
+    $d_name = $roww.d_name
+    $w_name = $roww.w_name
+    try {
+        $parameters = Invoke-PowerBIRestMethod -Url "groups/$w_id/datasets/$d_id/parameters" -Method Get | ConvertFrom-Json -ErrorAction Stop
+    }
+    catch {
+        Write-Host "Error: $w_id/datasets/$d_id"
+    }
     
     [PSCustomObject] @{
-        datasetid = $datasetid
-        workspace_id = $workspaceid
-        dataset_name = $dataset_name
-        workspace_name = $workspace_name
+        d_id = $d_id
+        w_id = $w_id
+        d_name = $d_name
+        w_name = $w_name
         p_value = $parameters.value
         }
 }
+
 Write-Host "Done!"
 
 Write-Host "Let's put all the parameter details into their own columns."
 $param_details = foreach($row in $param_values){
-    $datasetid = $row.datasetid
-    $workspace_id = $row.workspace_id
-    $dataset_name = $row.dataset_name
-    $workspace_name = $row.workspace_name
+    $d_id = $row.d_id
+    $w_id = $row.w_id
+    $d_name = $row.d_name
+    $w_name = $row.w_name
 
     foreach($inner in $row.p_value){
         $p_name = $inner.name
@@ -111,10 +119,10 @@ $param_details = foreach($row in $param_values){
         $p_currentValue = $inner.currentValue
 
          [PSCustomObject] @{
-            datasetid = $datasetid
-            workspace_id = $workspace_id
-            dataset_name = $dataset_name
-            workspace_name = $workspace_name
+            d_id = $d_id
+            w_id = $w_id
+            d_name = $d_name
+            w_name = $w_name
 
             p_name = $p_name
             p_type = $p_type
@@ -128,6 +136,6 @@ $result_count = $param_details.count
 Write-Host "Done! Exporting results ($result_count)."
 
 # Export results to file.
-$param_details | Select-Object -Property datasetid,dataset_name,workspace_id,workspace_name,p_name,p_type,p_isRequired,p_currentValue | Export-Csv -Path ".\dataset_parameters.csv"  -NoTypeInformation -Force
+$param_details | Select-Object -Property d_id,d_name,w_id,w_name,p_name,p_type,p_isRequired,p_currentValue | Export-Csv -Path ".\dataset_parameters.csv"  -NoTypeInformation -Force
 
 Write-Host "Output complete. Please find the file 'dataset_parameters.csv' in the local directory."
